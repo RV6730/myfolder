97c1711247368a7240ead29846bb94abaab42efb
sujay <ghsd@gmail.com>
2025-03-25 09:47:29 +0530

    swapping||hello.c

--- hello.c
+++ hello.c
@@ -1,45 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int ch, qty, total = 0;
-    printf("\n\tMENU CARD\n");
-    printf("\t\t1.COFFEE        Rs:15\n");
-    printf("\t\t2.TEA           Rs:10\n");
-    printf("\t\t3.COLD COFFEE   Rs:25\n");
-    printf("\t\t4.MILK SHAKE    Rs:50\n");
-    printf("\n Enter Your choice  : ");
-    scanf("%d", &ch);
+    int first, second, temp;
+    printf("Enter first number: ");
+    scanf("%d", &first);
+    printf("Enter second number: ");
+    scanf("%d", &second);
+
     
-    switch(ch) {
-        case 1:
-            printf("\nYou have selected Coffee\n");
-            printf("Enter The Qty : ");
-            scanf("%d", &qty);
-            total += qty * 15;
-            break;
-        case 2:
-            printf("\nYou have selected Tea\n");
-            printf("Enter The Qty : ");
-            scanf("%d", &qty);
-            total += qty * 10;
-            break;
-        case 3:
-            printf("\nYou have selected Cold Coffee\n");
-            printf("Enter The Qty : ");
-            scanf("%d", &qty);
-            total += qty * 25;
-            break;
-        case 4:
-            printf("\nYou have selected Milk Shake\n");
-            printf("Enter The Qty : ");
-            scanf("%d", &qty);
-            total += qty * 50;
-            break;
-        default:
-            printf("\nInvalid Product Selection\n");
-            break;
-    }
-    printf("\nTotal amount : %d", total);
+    temp = first;
+    
+    first = second;
+    
+    second = temp;
+
+    printf("\nAfter swapping, first number = %d\n", first);
+    printf("After swapping, second number = %d", second);
+
     return 0;
 }
 
