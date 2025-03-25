53317561f33cbefc4e747b4b1dae5e143e8ae531
sujay <ghsd@gmail.com>
2025-03-25 09:31:36 +0530

    hotel menue||hello.c

--- hello.c
+++ hello.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
+
 int main() {
-    int n, i;
-    unsigned long long fact = 1;
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-
-    // shows error if the user enters a negative integer
-    if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-    else {
-        for (i = 1; i <= n; ++i) {
-            fact *= i;
-        }
-        printf("Factorial of %d = %llu", n, fact);
+    int ch, qty, total = 0;
+    printf("\n\tMENU CARD\n");
+    printf("\t\t1.COFFEE        Rs:15\n");
+    printf("\t\t2.TEA           Rs:10\n");
+    printf("\t\t3.COLD COFFEE   Rs:25\n");
+    printf("\t\t4.MILK SHAKE    Rs:50\n");
+    printf("\n Enter Your choice  : ");
+    scanf("%d", &ch);
+    
+    switch(ch) {
+        case 1:
+            printf("\nYou have selected Coffee\n");
+            printf("Enter The Qty : ");
+            scanf("%d", &qty);
+            total += qty * 15;
+            break;
+        case 2:
+            printf("\nYou have selected Tea\n");
+            printf("Enter The Qty : ");
+            scanf("%d", &qty);
+            total += qty * 10;
+            break;
+        case 3:
+            printf("\nYou have selected Cold Coffee\n");
+            printf("Enter The Qty : ");
+            scanf("%d", &qty);
+            total += qty * 25;
+            break;
+        case 4:
+            printf("\nYou have selected Milk Shake\n");
+            printf("Enter The Qty : ");
+            scanf("%d", &qty);
+            total += qty * 50;
+            break;
+        default:
+            printf("\nInvalid Product Selection\n");
+            break;
     }
-
+    printf("\nTotal amount : %d", total);
     return 0;
 }
 
